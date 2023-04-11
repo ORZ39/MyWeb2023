e4765e25d3ae4ee11e38cd19d0ff5d6396ea1ecd
ORZ39 <130308109+ORZ39@users.noreply.github.com>
2023-04-12 02:17:25 +0300

    Update signup.php

--- signup.php
+++ signup.php
@@ -1,33 +1,73 @@
 <?php
 session_start();
-
 if(isset($_SESSION['username'])) {
     // User already logged in
     header("Location: dashboard.php");
     exit();
 }
 
-if($_SERVER['REQUEST_METHOD'] == 'POST') {
-    require_once('database.php');
+$error = '';
 
+if($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Get input values from form
+    $username = $_POST['username'];
+    $password = password_hash($_POST['password'], PASSWORD_DEFAULT);
+
+    // Connect to database
+    require_once('database.php');
     $conn = connect_to_db();
 
-    $username = mysqli_real_escape_string($conn, $_POST['username']);
-    $password = mysqli_real_escape_string($conn, $_POST['password']);
+    // Check if username is already taken
+    $sql = "SELECT * FROM users WHERE username = ?";
+    $stmt = $conn->prepare($sql);
+    $stmt->bind_param("s", $username);
+    $stmt->execute();
+    $result = $stmt->get_result();
 
-    $hash = password_hash($password, PASSWORD_DEFAULT);
-
-    $query = "INSERT INTO users (username, password) VALUES ('$username', '$hash')";
-    $result = mysqli_query($conn, $query);
-
-    if($result) {
-        $_SESSION['username'] = $username;
-        header("Location: dashboard.php");
-        exit();
+    if($result->num_rows > 0) {
+        // Username already taken
+        $error = "Username already taken";
+    } else {
+        // Username is available, create new user
+        $sql = "INSERT INTO users (username, password) VALUES (?, ?)";
+        $stmt = $conn->prepare($sql);
+        $stmt->bind_param("ss", $username, $password);
+        if($stmt->execute()) {
+            // User created successfully, start session and redirect to dashboard
+            $_SESSION['username'] = $username;
+            header("Location: dashboard.php");
+            exit();
+        } else {
+            // Error creating user
+            $error = "Error creating user";
+        }
     }
-
-    $error_message = "Error creating account.";
+    $stmt->close();
+    $conn->close();
 }
-
 ?>
 
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Sign Up</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+    <div class="container">
+        <h2>Sign Up</h2>
+        <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
+            <label for="username">Username:</label>
+            <input type="text" id="username" name="username" required>
+            <br>
+            <label for="password">Password:</label>
+            <input type="password" id="password" name="password" required>
+            <br>
+            <span class="error"><?php echo $error; ?></span>
+            <br>
+            <input type="submit" value="Submit">
+        </form>
+        <p>Already have an account? <a href="login.php">Log in here</a>.</p>
+    </div>
+</body>
+</html>
