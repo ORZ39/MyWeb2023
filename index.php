1677a4d1037b1ef0f19227e9694b8db7ff7f5157
ORZ39 <130308109+ORZ39@users.noreply.github.com>
2023-04-12 02:12:20 +0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>My Website</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+    <div class="container">
+        <h1>Welcome to My Website</h1>
+        <p>This is the landing page for my website. Please login or sign up to continue.</p>
+        <a href="login.php">Login</a> or <a href="signup.php">Sign Up</a>
+    </div>
+</body>
+</html>
