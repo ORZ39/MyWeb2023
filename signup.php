683ef13de262ccf766ad01d5f76cad3427385fed
ORZ39 <130308109+ORZ39@users.noreply.github.com>
2023-04-12 02:09:41 +0300

    Update signup.php

--- signup.php
+++ signup.php
@@ -1,67 +1,33 @@
 <?php
 session_start();
 
- // Connect to database
-    $conn = new mysqli("mywebserver2023.mysql.database.azure.com", "orz1920", "R.o.123456789", "mydatabase");
-
-    // Check connection
-    if($conn->connect_error) {
-        die("Connection failed: " . $conn->connect_error);
-    }
-
 if(isset($_SESSION['username'])) {
     // User already logged in
     header("Location: dashboard.php");
     exit();
 }
 
-if(isset($_POST['username']) && isset($_POST['password'])) {
-    // Form submitted, try to log in
-    $username = $_POST['username'];
-    $password = $_POST['password'];
+if($_SERVER['REQUEST_METHOD'] == 'POST') {
+    require_once('database.php');
 
-   
+    $conn = connect_to_db();
 
-    // Query database for user
-    $query = "SELECT * FROM users WHERE username='$username' AND password='$password'";
-    $result = $conn->query($query);
+    $username = mysqli_real_escape_string($conn, $_POST['username']);
+    $password = mysqli_real_escape_string($conn, $_POST['password']);
 
-    if($result->num_rows > 0) {
-        // User found, log them in
+    $hash = password_hash($password, PASSWORD_DEFAULT);
+
+    $query = "INSERT INTO users (username, password) VALUES ('$username', '$hash')";
+    $result = mysqli_query($conn, $query);
+
+    if($result) {
         $_SESSION['username'] = $username;
         header("Location: dashboard.php");
         exit();
-    } else {
-        // Invalid username/password
-        $error = "Invalid username or password.";
     }
 
-    // Close connection
-    $conn->close();
+    $error_message = "Error creating account.";
 }
+
 ?>
 
-<!DOCTYPE html>
-<html>
-<head>
-    <title>Login</title>
-    <link rel="stylesheet" type="text/css" href="style.css">
-</head>
-<body>
-    <div class="container">
-        <div class="form">
-            <h2>Login</h2>
-            <?php if(isset($error)) { ?>
-                <p class="error"><?php echo $error; ?></p>
-            <?php } ?>
-            <form method="post">
-                <label for="username">Username:</label>
-                <input type="text" name="username" required>
-                <label for="password">Password:</label>
-                <input type="password" name="password" required>
-                <input type="submit" value="Login">
-            </form>
-        </div>
-    </div>
-</body>
-</html>
